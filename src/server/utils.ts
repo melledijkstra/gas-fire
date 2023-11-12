9cc285ad4b846cb50a56f4a436406481dfaac8f1
Melle Dijkstra <melle.dykstra@gmail.com>
2023-11-12 16:20:23 +0100

    Remove the need of configuration file, accounts are extracted directly from the spreadsheet (#19)

--- src/server/utils.ts
+++ src/server/utils.ts
@@ -1,4 +1,4 @@
-import { Table, InputColumn } from './types';
+import { Table, InputColumn, StrategyOption } from './types';
 
 export const SOURCE_SHEET_ID = 1093484485;
 
@@ -105,5 +105,32 @@ export class Utils {
         .reverse();
       return data;
     };
+  }
+
+  static getBankAccounts(): Record<string, string> {
+    // this range contains the ibans only
+    const ibans = FireSpreadsheet.getRangeByName('accounts');
+    // we also need to include the labels
+    const accounts = ibans
+      ?.offset(0, -1, ibans.getLastRow(), 2)
+      .getValues()
+      // make sure not to include empty rows
+      .filter((row) => row.some((cell) => cell !== '' && cell !== null));
+
+    if (!accounts?.length) {
+      return {}; // return empty list of bank accounts if none setup
+    }
+
+    // convert the list to an object to easy work with it
+    return accounts.reduce((obj, account) => {
+      const [id, iban] = account;
+      obj[id.toUpperCase()] = iban;
+      return obj;
+    }, {});
+  }
+
+  static getBankIban(bank: string): string {
+    const bankAccounts = Utils.getBankAccounts();
+    return bankAccounts?.[bank] ?? '';
   }
 }
