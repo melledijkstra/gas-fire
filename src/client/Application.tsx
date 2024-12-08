3ff7da13e56629fa3ac62238f07c72e7e928ca27
Melle Dijkstra <melle.dijkstra@adidas.com>
2024-12-08 17:13:33 +0100

    tailwind initial setup

--- src/client/Application.tsx
+++ src/client/Application.tsx
@@ -2,6 +2,7 @@ import { ScopedCssBaseline } from '@mui/material';
 import { ThemeProvider, createTheme } from '@mui/material';
 import { colors } from '@mui/material';
 import { ReactNode } from 'react';
+import './index.css';
 
 const { green, blue } = colors;
 
@@ -27,9 +28,11 @@ export const Application = ({ children }: { children: ReactNode }) => {
     },
   });
 
-  return (
-    <ScopedCssBaseline>
-      <ThemeProvider theme={theme}>{children}</ThemeProvider>
-    </ScopedCssBaseline>
-  );
+  return <p className="border-2 border-blue-500">Test Test</p>;
+
+  // return (
+  //   <ScopedCssBaseline>
+  //     <ThemeProvider theme={theme}>{children}</ThemeProvider>
+  //   </ScopedCssBaseline>
+  // );
 };
---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/client/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
