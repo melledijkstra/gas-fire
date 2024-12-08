90e8327c89108c77dfecbacb60cdaf9a6780b881
Melle Dijkstra <melle.dijkstra@adidas.com>
2024-12-08 16:55:22 +0100

    [WIP] removed a lot of old static configuration and converted them to be dynamic (auto fill, column mapping, auto categorization flag)

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,3 +1,5 @@
+import { Config } from './config';
+
 // Functions to setup the initial UI
 export * from './ui';
 
@@ -6,3 +8,7 @@ export * from './remote-calls';
 
 // Custom functions that can be used within the Spreadsheet UI
 export { MD5, GET_PROJECT_VERSION } from './exposed_functions';
+
+export function test() {
+  Config._loadConfigurations();
+}
