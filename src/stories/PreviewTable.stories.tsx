fe42519d78d3617218fb6ef3995e279b0e43e135
Melle Dijkstra <melle.dijkstra@adidas.com>
2025-03-06 20:46:11 +0100

    upgrade storybook + fix import stories

--- src/stories/PreviewTable.stories.tsx
+++ src/stories/PreviewTable.stories.tsx
@@ -1,6 +1,7 @@
 import type { Meta, StoryObj } from '@storybook/react';
 import { PreviewTable } from '@/client/import-dialog/components/PreviewTable';
 import { n26ImportMock } from '@/fixtures/n26';
+import { ImportContextProvider } from '@/client/import-dialog/context/import-context';
 
 const meta: Meta<typeof PreviewTable> = {
   title: 'Dialogs/Import',
@@ -14,4 +15,12 @@ export default meta;
 
 type Story = StoryObj<typeof PreviewTable>;
 
-export const Table: Story = {};
+export const Table: Story = {
+  render: (args) => {
+      return (
+        <ImportContextProvider setStatusText={() => {}}>
+          <PreviewTable />
+        </ImportContextProvider>
+      )
+    }
+};
